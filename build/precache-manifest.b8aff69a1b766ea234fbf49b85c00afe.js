dd00cbfac4ba5f438ad5d0525e92fe23a623de67
Swati <swatisw45@gmail.com>
2023-07-26 15:01:46 +0530

    1st

---
+++ build/precache-manifest.b8aff69a1b766ea234fbf49b85c00afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f78cdd8e3099739833735420c3a30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268692fbf95b5e6dbb96",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268692fbf95b5e6dbb96",
+    "url": "/static/js/main.5e0ad74c.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
